7cff147bbf57d41b57edea85195bf309221ce456
xiaoyu.han <xiaoyu.han@mfashion.com.cn>
2017-10-26 18:43:05 +0800

    丰富格式化

--- src/Formatter/PoetryFormatter.php
+++ src/Formatter/PoetryFormatter.php
@@ -6,19 +6,69 @@ class PoetryFormatter
 {
     private $poetry;
 
+    private $separator;
+
     public function __construct($poetry)
     {
         $this->poetry = $poetry;
+        $this->separator = '********************';
     }
 
-    public function formatter()
+    /**
+     * 按照逗号、句号分割诗句
+     *
+     * @return array
+     */
+    public function splitPoetry()
     {
+        $res = ['status' => false, 'msg' => '', 'data' => []];
         $pattern = '/[,.]/';
         $data = preg_split($pattern, $this->poetry->get());
+
         if ($data === false) {
-            echo '哎呀 出错了，这首诗有问题';return;
+            $res['msg'] = 'php 函数罢工啦';
+            return $res;
+        } else if (empty($res)) {
+            $res['msg'] = '这诗有问题';
+            return $res;
         }
-        $res = implode("\n", $data);
+
+        $res['status'] = true;
+        $res['data'] = $data;
+        return $res;
+    }
+
+    /**
+     * 简单格式化
+     *
+     * @return string
+     */
+    public function format()
+    {
+        $poetry = $this->splitPoetry();
+        if ($poetry['status'] == false) {
+            return $poetry['msg'];
+        }
+        $res = implode("\n", $poetry['data']);
+        return $res;
+    }
+
+    /**
+     * 带有分隔符的格式化，并突出标题
+     *
+     * @return string
+     */
+    public function formatWithSeparator()
+    {
+        $poetry = $this->splitPoetry();
+        if ($poetry['status'] == false) {
+            return $poetry['msg'];
+        }
+        $poetry = $poetry['data'];
+        $title = array_shift($poetry);
+        $title = '[ ' . $title . ' ]';
+        $content = implode("\n", $poetry);
+        $res = $this->separator . "\n\n" . $title . "\n\n" . $content;
         return $res;
     }
 }
--- tests/Formatter/PoetryFormatterTest.php
+++ tests/Formatter/PoetryFormatterTest.php
@@ -6,11 +6,31 @@ use Daxia\Poetry\Generator\PoetryGenerator;
 
 class PoetryFormatterTest extends PHPUnit_Framework_TestCase
 {
-    public function testFormatter()
+
+    public function testSplitPoetry()
     {
         $generator = new PoetryGenerator();
         $formatter = new PoetryFormatter($generator);
-        $poetry = $formatter->formatter();
+        $poetry = $formatter->splitPoetry();
+        $this->assertInternalType('array', $poetry);
+        $this->assertArrayHasKey('status', $poetry);
+        $this->assertArrayHasKey('msg', $poetry);
+        $this->assertArrayHasKey('data', $poetry);
+    }
+
+    public function testFormat()
+    {
+        $generator = new PoetryGenerator();
+        $formatter = new PoetryFormatter($generator);
+        $poetry = $formatter->format();
+        $this->assertInternalType('string', $poetry);
+    }
+
+    public function testFormatWithSeparator()
+    {
+        $generator = new PoetryGenerator();
+        $formatter = new PoetryFormatter($generator);
+        $poetry = $formatter->formatWithSeparator();
         $this->assertInternalType('string', $poetry);
     }
 }
